d4ce05f15081d2bf9d8445b44a3cab0bc02a453c
ahmad-186 <ahmadshahzad007k@gmail.com>
2026-09-15 16:20:01 +0500

    DAY 04: Matrix -> Spiral Matrix

---
+++ spiral_matrix_problem.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+void spiralMAtrix(int matrix[][4], int n, int m) {
+
+    int srow = 0, scol = 0;
+    int erow = n-1, ecol = m-1;
+
+    while (srow <= erow && scol <= ecol)
+    {
+        // Top
+        for(int j = scol; j <=ecol; j++){
+            cout << matrix[srow][j] << " ";
+        }
+
+        // Right
+        for(int i = srow + 1; i <= erow; i++) {
+            cout << matrix[i][ecol] << " ";
+        }
+
+        // Bottom
+        for(int j = ecol-1; j >= scol; j--) {
+            if(srow == erow) {
+                break;
+            }
+            cout << matrix[erow][j] << " ";
+        }
+
+        // Left
+        for(int i = erow-1; i > srow; i--) {
+            if(scol == ecol) {
+                break;
+            }
+            cout << matrix[i][scol] << " ";
+        }
+
+        srow++; scol++;
+        erow--; ecol--;
+
+    }
+    
+    cout << endl;
+    
+}
+
+int main() {
+
+    int matrix[4][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 17}};
+
+    spiralMAtrix(matrix, 4, 4);
+
+    int matrix2[3][4] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12}};
+
+    spiralMAtrix(matrix2, 3, 4);
+
+    return 0;
+}
